e50b3bee140b91409349a3e0c0bd7a1239df722f
AKarakutsa <andreikarakutsa@gmail.com>
2023-08-22 14:39:05 +0300

    Added comments to gateway-service module

--- gateway-service/src/main/java/com/microservices/demo/gateway/service/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/microservices/demo/gateway/service/config/GatewayConfig.java
@@ -27,23 +27,29 @@ public class GatewayConfig {
 
     private static final String HEADER_FOR_KEY_RESOLVER = "Authorization";
 
+    // Constructor injection of configuration data
     public GatewayConfig(GatewayServiceConfigData configData) {
         this.gatewayServiceConfigData = configData;
     }
 
+    // Bean definition for KeyResolver, used in rate limiting
     @Bean(name = "authHeaderResolver")
     KeyResolver userKeyResolver() {
+        // Retrieves the value of the "Authorization" header from the request
         return exchange -> Mono.just(Objects.requireNonNull(exchange
                 .getRequest().getHeaders().getFirst(HEADER_FOR_KEY_RESOLVER)));
     }
 
+    // Customizer for ReactiveResilience4JCircuitBreakerFactory, used to provide resilience patterns like circuit breaking
     @Bean
     Customizer<ReactiveResilience4JCircuitBreakerFactory> circuitBreakerFactoryCustomizer() {
         return reactiveResilience4JCircuitBreakerFactory ->
                 reactiveResilience4JCircuitBreakerFactory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                        // Configuring TimeLimiter to set a timeout duration for the circuit breaker
                         .timeLimiterConfig(TimeLimiterConfig.custom()
                                 .timeoutDuration(Duration.ofMillis(gatewayServiceConfigData.getTimeoutMs()))
                                 .build())
+                        // Configuring CircuitBreaker to detect failures and tripping the circuit
                         .circuitBreakerConfig(CircuitBreakerConfig.custom()
                                 .failureRateThreshold(gatewayServiceConfigData.getFailureRateThreshold())
                                 .slowCallRateThreshold(gatewayServiceConfigData.getSlowCallRateThreshold())
@@ -58,5 +64,5 @@ public class GatewayConfig {
                                 .build())
                         .build());
     }
-
 }
+
